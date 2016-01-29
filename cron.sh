a1d0459e9718de353de66f2dcac3479d8f9d11ef
lemoer <leo.moerlein@gmail.com>
2016-01-29 22:47:03 +0100

    Update cron-example to new syntax.

--- cron.sh
+++ cron.sh
@@ -2,9 +2,10 @@
 
 cd "$(dirname $0)/handler"
 
-sh if_stats.sh freifunk
-sh if_stats.sh bat0
-sh ping.sh 8.8.8.8
-sh ping.sh sponge.gatrobe.de
+sh uptime.sh
+sh if_stats.sh enp0s25
+sh if_stats.sh wlp3s0
+ping -c 5 -i 0.2 8.8.8.8 | sh format_ping.sh "google dns"
+ping -c 5 -i 0.2 sponge.gatrobe.de | sh format_ping.sh "sponge"
 
 python3 fastd.py /var/run/fastd.sock
