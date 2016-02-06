d614c953d7fc74c78b9cc0870b9e7fbb1b564998
lemoer <leo.moerlein@gmail.com>
2016-02-06 16:11:57 +0100

    Fix handler: ping_roundtrip printed no value.

--- handler/format_ping.sh
+++ handler/format_ping.sh
@@ -7,4 +7,4 @@ package_loss=$(echo "$result" | grep "packet loss" | cut -d" " -f 6 | tr -d "%")
 roundtrip=$(echo "$result" | grep "rtt" | cut -d " " -f 4 | cut -d "/" -f 2)
 
 echo ping_package_loss{descr=\"$descr\"} $package_loss
-echo ping_roundtrip{descr=\"$descr\"} $package_roundtrip
+echo ping_roundtrip{descr=\"$descr\"} $roundtrip
