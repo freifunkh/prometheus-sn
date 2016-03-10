22e40da0fb79fc54e55c090ec63cff36e1af80fe
lemoer <leo.moerlein@gmail.com>
2016-03-11 00:44:43 +0100

    Default gen.sh script is now adjusted to freifunk gw.

--- gen.sh
+++ gen.sh
@@ -4,7 +4,10 @@ cd "$(dirname $0)/handler"
 
 sh uptime.sh
 sh if_stats.sh
-ping -c 5 -i 0.2 8.8.8.8 | sh format_ping.sh "google dns"
-ping -c 5 -i 0.2 sponge.gatrobe.de | sh format_ping.sh "sponge"
+ping -c 5 -i 0.2 8.8.8.8 | sh format_ping.sh "google-dns"
+ping -c 5 -i 0.2 freifunk1.internetz.me | sh format_ping.sh "internetz.me"
 
-python3 fastd.py /var/run/fastd.sock
+# all fastd status sockets
+for f in /var/run/fastd*.sock; do
+    python3 fastd.py $f
+done
