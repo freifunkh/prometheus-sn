a6bc3deb4a694dba508a5e63901f48195697e194
lemoer <leo.moerlein@gmail.com>
2016-03-11 00:56:56 +0100

    Default gen.sh script now queries just one fastd.sock.

--- gen.sh
+++ gen.sh
@@ -7,7 +7,5 @@ sh if_stats.sh
 ping -c 5 -i 0.2 8.8.8.8 | sh format_ping.sh "google-dns"
 ping -c 5 -i 0.2 freifunk1.internetz.me | sh format_ping.sh "internetz.me"
 
-# all fastd status sockets
-for f in /var/run/fastd*.sock; do
-    python3 fastd.py $f
-done
+# fastd
+python fastd.py /var/run/fastd.ffh-mesh-vpn.sock
