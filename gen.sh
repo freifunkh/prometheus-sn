592461162bf59704c9bb614e174b96cc83bf992d
lemoer <leo.moerlein@gmail.com>
2016-03-11 01:21:54 +0100

    Default gen.sh: added handler/load.sh

--- gen.sh
+++ gen.sh
@@ -3,6 +3,7 @@
 cd "$(dirname $0)/handler"
 
 sh uptime.sh
+sh load.sh
 sh if_stats.sh
 ping -c 5 -i 0.2 8.8.8.8 | sh format_ping.sh "google-dns"
 ping -c 5 -i 0.2 freifunk1.internetz.me | sh format_ping.sh "internetz.me"
