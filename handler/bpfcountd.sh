fdea11707b719e06574917cf916fc6f9746d90d6
lemoer <git@irrelefant.net>
2016-03-19 13:18:47 +0100

    handler/bpfcountd.sh: Added iface label.

--- handler/bpfcountd.sh
+++ handler/bpfcountd.sh
@@ -3,12 +3,13 @@
 usock_path="$1"
 
 stats=$(nc -U "$usock_path")
+iface=$(echo "$usock_path" | awk -F. '{print $(NF-1)}')
 
 for line in $stats; do
 	identifier=$(echo "$line" | cut -d ":" -f 1)
 	bytes=$(echo "$line" | cut -d ":" -f 2)
 	packets=$(echo "$line" | cut -d ":" -f 3)
 
-	echo "bpfcountd_packets{$identifier} $packets"
-	echo "bpfcountd_bytes{$identifier} $bytes"
+	echo "bpfcountd_packets{iface=\"$iface\",$identifier} $packets"
+	echo "bpfcountd_bytes{iface=\"$iface\",$identifier} $bytes"
 done
